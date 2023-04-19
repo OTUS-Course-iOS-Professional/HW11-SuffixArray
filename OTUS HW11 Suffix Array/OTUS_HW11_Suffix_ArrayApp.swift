76898d8b903026577eb53911beb78298c674b4a9
nerzul88 <nerzul.88@gmail.com>
2023-04-19 22:25:10 +0300

    Initial commit

---
+++ OTUS HW11 Suffix Array/OTUS_HW11_Suffix_ArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_HW11_Suffix_ArrayApp.swift
+//  OTUS HW11 Suffix Array
+//
+//  Created by Александр Касьянов on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_HW11_Suffix_ArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
